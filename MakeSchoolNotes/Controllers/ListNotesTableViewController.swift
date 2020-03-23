cd28785e4cea2b7c2137afd12a58bc1eb874a7c0
Stephanie Ballard <user.name>
2020-03-22 21:50:32 -0700

    section 8 completed

--- MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -10,22 +10,40 @@ import UIKit
 
 class ListNotesTableViewController: UITableViewController {
     
+    var notes = [Note]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func unwindWithSegue(_ segue: UIStoryboardSegue) {
+        
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // 1
-        return 10
+        return notes.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 2
+        let note = notes[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
-        cell.noteTitleLabel.text = "note's title"
-        cell.noteModificationTimeLabel.text = "note's modification time"
+        cell.noteTitleLabel.text = note.title
+        cell.noteModificationTimeLabel.text = note.modificationTime.convertToString()
         return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let identifier = segue.identifier else { return }
+        switch identifier {
+        case "displayNote":
+            print("note cell tapped")
+        case "addNote":
+            print("create note bar button item tapped")
+        default:
+            print("unexpected segue indentifier")
+        }
     }
 }
 
---
+++ MakeSchoolNotes/Models/Notes.swift
@@ -0,0 +1,18 @@
+//
+//  Notes.swift
+//  MakeSchoolNotes
+//
+//  Created by Stephanie Ballard on 3/22/20.
+//  Copyright © 2020 MakeSchool. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+
+    // MARK: - Properties
+
+    var title = ""
+    var content = ""
+    var modificationTime = Date()
+}
